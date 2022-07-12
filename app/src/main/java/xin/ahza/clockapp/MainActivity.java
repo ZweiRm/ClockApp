6f0931585dcb6ab4aa6d76bb753a520f5e0495d6
huangduo <huangduo@xiaomi.com>
2022-07-12 19:02:00 +0800

    Develop the resume/stop  and move function of  FloatingWindowService

--- app/src/main/java/xin/ahza/clockapp/MainActivity.java
+++ app/src/main/java/xin/ahza/clockapp/MainActivity.java
@@ -2,21 +2,84 @@ package xin.ahza.clockapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 import xin.ahza.clockapp.service.FloatingWindowService;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean hasBind = false;
+
+    private Button mFloatingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent();
-        intent.setClass(this, FloatingWindowService.class);
-        startService(intent);
-//        this.finish();
+        mFloatingBtn = findViewById(R.id.floatingBtn);
+        mFloatingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveTaskToBack(true);
+                Intent intent = new Intent(MainActivity.this, FloatingWindowService.class);
+
+                hasBind = bindService(intent, mClockServiceConnection, Context.BIND_AUTO_CREATE);
+            }
+        });
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (isServiceRunning(this, FloatingWindowService.class.getName())) {
+            Intent stopServiceIntent = new Intent(this, FloatingWindowService.class);
+            stopService(stopServiceIntent);
+            unbindService(mClockServiceConnection);
+        }
+    }
+
+    ServiceConnection mClockServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            // 获取服务操作对象
+            FloatingWindowService.FloatBinder binder = (FloatingWindowService.FloatBinder) service;
+            binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+
+
+    public static boolean isServiceRunning(Context mContext, String className) {
+        boolean isRunning = false;
+        ActivityManager activityManager = (ActivityManager)
+                mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> serviceList = activityManager.getRunningServices(30);
+        if (!(serviceList.size() > 0)) {
+            return false;
+        }
+        Log.e("OnlineService：", className);
+        for (int i = 0; i < serviceList.size(); i++) {
+            Log.e("serviceName：", serviceList.get(i).service.getClassName());
+            if (serviceList.get(i).service.getClassName().contains(className) == true) {
+                isRunning = true;
+                break;
+            }
+        }
+        return isRunning;
+
     }
 }
--- app/src/main/java/xin/ahza/clockapp/service/FloatingWindowService.java
+++ app/src/main/java/xin/ahza/clockapp/service/FloatingWindowService.java
@@ -3,10 +3,14 @@ package xin.ahza.clockapp.service;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.os.Binder;
 import android.os.IBinder;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 
+import xin.ahza.clockapp.MainActivity;
 import xin.ahza.clockapp.view.ClockViewWithHandler;
 
 public class FloatingWindowService extends Service {
@@ -15,13 +19,58 @@ public class FloatingWindowService extends Service {
     private WindowManager mWindowManager;
     private WindowManager.LayoutParams mLayoutParams;
     private ClockViewWithHandler mHandlerClockView;
-    private boolean flag = true;
+
+    private int mTouchStartX, mTouchStartY, mTouchCurrentX, mTouchCurrentY; // 开始触控的坐标，移动时的坐标（相对于屏幕左上角的坐标）
+    private int mStartX, mStartY, mStopX, mStopY;   // 开始时的坐标和结束时的坐标（相对于自身控件的坐标）
+    private boolean isMove; // 判断悬浮窗口是否移动，这里做个标记，防止移动后松手触发了点击事件
 
     @Override
     public void onCreate() {
         super.onCreate();
+    }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mWindowManager.removeView(mHandlerClockView);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        initFloating();
+        initWindow();
+        return new FloatBinder();
+    }
+
+    public class FloatBinder extends Binder {
+        public FloatingWindowService getService() {
+            return FloatingWindowService.this;
+        }
+    }
+
+    private void initFloating() {
+        mHandlerClockView = new ClockViewWithHandler(this);
+        mHandlerClockView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FloatingWindowService.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                mWindowManager.removeView(mHandlerClockView);
+            }
+        });
+        mHandlerClockView.setOnTouchListener(new FloatingListener());
+    }
+
+    private void initWindow() {
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
         mLayoutParams = new WindowManager.LayoutParams();
         mLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
         mLayoutParams.format = PixelFormat.TRANSLUCENT;
@@ -32,29 +81,47 @@ public class FloatingWindowService extends Service {
         mLayoutParams.x = 0;
         mLayoutParams.y = 300;
 
-        mHandlerClockView = new ClockViewWithHandler(this);
+        mWindowManager.addView(mHandlerClockView, mLayoutParams);
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (flag) {
-            flag = false;
-            mWindowManager.addView(mHandlerClockView, mLayoutParams);
+    private class FloatingListener implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getAction();
+
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    isMove = false;
+                    mTouchStartX = (int) event.getRawX();
+                    mTouchStartY = (int) event.getRawY();
+                    mStartX = (int) event.getX();
+                    mStartY = (int) event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    mTouchCurrentX = (int) event.getRawX();
+                    mTouchCurrentY = (int) event.getRawY();
+
+                    mLayoutParams.x += mTouchCurrentX - mTouchStartX;
+                    mLayoutParams.y += mTouchCurrentY - mTouchStartY;
+                    mWindowManager.updateViewLayout(mHandlerClockView, mLayoutParams);
+
+                    mTouchStartX = mTouchCurrentX;
+                    mTouchStartY = mTouchCurrentY;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    mStopX = (int) event.getX();
+                    mStopY = (int) event.getY();
+
+                    if (Math.abs(mStartX - mStopX) >= 1 || Math.abs(mStartY - mStopY) >= 1) {
+                        isMove = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            // 如果是移动事件不触发OnClick事件，防止移动的时候一放手形成点击事件
+            return isMove;
         }
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        if (mHandlerClockView.getParent() != null) {
-            mWindowManager.removeView(mHandlerClockView);
-        }
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
     }
 }
